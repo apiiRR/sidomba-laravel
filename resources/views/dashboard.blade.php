8ee15f6521d8b74886ef64ea8514923536ed05cf
Rafi Ramadhana <ramadhanarafi437@gmail.com>
2025-07-30 16:47:25 +0700

    fix : sideToggleTop

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -280,9 +280,6 @@
   <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
   <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
-  <!-- Custom scripts for all pages-->
-  <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
-
 
   <!-- Page level plugins -->
   <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -170,6 +170,9 @@
   <!-- Core plugin JavaScript-->
   <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
+  <!-- Custom scripts for all pages-->
+  <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+
   {{--
   <!-- Custom scripts for all pages-->
   <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
